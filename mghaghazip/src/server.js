78cc33a02ed200302ea08eed6b8ed82c6e4ea084
Ahmed Mahmoud <ahmedplxyman@gmail.com>
2026-06-22 17:27:35 +0300

    Delete mghaghazip directory

--- mghaghazip/src/server.js
+++
@@ -1,9 +0,0 @@
-const env = require('./config/env');
-const app = require('./app');
-const { startBackupScheduler } = require('./db/backup');
-
-startBackupScheduler();
-
-app.listen(env.port, () => {
-  console.log(`✔ Mghagha Store server running on http://localhost:${env.port}`);
-});
